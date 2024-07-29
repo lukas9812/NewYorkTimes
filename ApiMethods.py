0e995c7d2841407d29d50559e6b7b0d911383b2a
lsrovnal <Lukas.Srovnal.extern@buerse-stuttgart.de>
2024-07-29 22:10:55 +0200

    init_commit

---
+++ ApiMethods.py
@@ -0,0 +1,10 @@
+﻿class ApiMethods:
+
+    def get_book_properties(apiData, property):
+        properties = []
+        for item in apiData:
+            properties.append(item[property])
+
+        return properties
+        
+    
---
+++ CallApi.py
@@ -0,0 +1,26 @@
+﻿import requests
+import config
+
+import ApiMethods
+
+# Define the API endpoint and your API key
+url = "https://api.nytimes.com/svc/books/v3/reviews.json?author=Stephen+King&api-key={}".format(config.api_key)
+
+
+# Make the GET request
+response = requests.get(url)
+
+# Check if the request was successful
+if response.status_code == 200:
+    # Process the response
+    data = response.json()['results']
+    propsCollection = ApiMethods.get_book_properties(data, 'book_title')
+    
+    for property in propsCollection:
+        print(property)
+else:
+    print(f"Request failed with status code {response.status_code}: {response.text}")
+    
+
+
+
